e76dd8177682a1584fcb14d1251477eea2470a41
Renato Crovella <94860809+RenatoCrovella@users.noreply.github.com>
2023-12-18 19:23:03 -0300

    Add files via upload

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,44 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { React, useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "../src/component/Header";
-import Footer from '../src/component/Footer';
-import Index from '../src/component/Index';
-import NotFound from '../src/component/NotFound';
+import Footer from "../src/component/Footer";
+import Index from "../src/component/Index";
+import CreatePage from "../src/component/CreatePage";
+import NotFound from "../src/component/NotFound";
+import DetailedBlogView from "./component/DetailedBlogView";
 
 const App = () => {
+  const [isLogged, setIsLogged] = useState(false);  //MUDAR PARA FALSE DEPOIS!
+
+  console.log("App.js");
   return (
     <Router>
       <div>
-        <Header />
+        <Header isLogged={isLogged} />
 
         <Routes>
           <Route
-            exact
             path="/"
-            element={<Index route="/" />} // Pass route prop for '/'
+            element={<Index route="/" />} //Pass route prop for '/'
           />
           <Route
-            path="/school"
-            element={<Index route="/school" />} // Pass route prop for '/school'
+            path="/technical"
+            element={<Index route="/technical" />} // Pass route prop for '/technical' #NOT USED YET
           />
           <Route
-            path="/college"
-            element={<Index route="/college" />} // Pass route prop for '/college'
+            path="/other"
+            element={<Index route="/other" />} // Pass route prop for '/other' #NOT USED YET
+          />
+          <Route 
+            path="/create" 
+            element={<CreatePage isLogged={isLogged} />} />
+          <Route
+            path="/edit/:blogId"
+            element={<DetailedBlogView isLogged={isLogged} isEditing={true} />}
+          />
+          <Route
+            path="/read/:blogId"
+            element={<DetailedBlogView isLogged={isLogged} isEditing={false} />}
           />
           <Route
             path="/*"
